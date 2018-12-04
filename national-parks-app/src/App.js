da642bc91cfe4d27ba8d9dc023346112a275b8a9
myost <mayo9279@colorado.edu>
2018-12-04 10:43:41 -0700

    added css

--- national-parks-app/src/App.js
+++ national-parks-app/src/App.js
@@ -1,26 +1,13 @@
 // adding a comment test - madi
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import parks from './components/parks.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <parks />
       </div>
     );
   }
