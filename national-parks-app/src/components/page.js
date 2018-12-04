bb8a4ac0176dced1be807b48a3687e79b827e0c5
myost <mayo9279@colorado.edu>
2018-12-04 15:15:39 -0700

    Changed files for styling

--- national-parks-app/src/components/page.js
+++ national-parks-app/src/components/page.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Search from '../components/search';
+import Parks from './parks.js'
 
 class Page extends React.Component {
     render() {
         return (
             <div>
                 <Search />
+                <Parks />
             </div>
         );
     }
--- national-parks-app/src/components/parks.js
+++ national-parks-app/src/components/parks.js
@@ -8,8 +8,10 @@ export default class Parks extends React.Component {
 		return(
 			<div className="parks">
 				<ParkTitle title="Rocky Mountain National Park" />
-				<ParkImg imagelink="https://rockymountainnationalpark.com/sites/all/themes/rmnp/images/fall-foliage-rocky-mountain-national-park.jpg" />
-				<ParkDesc descript="Lorem ipsum dolor amet umami laboris microdosing copper mug cillum, ethical cornhole. Deserunt XOXO vaporware, kombucha drinking vinegar cronut street art crucifix. Single-origin coffee glossier blue bottle jean shorts iPhone echo park, iceland stumptown laboris tofu dreamcatcher vape. Nulla messenger bag everyday carry fingerstache, asymmetrical try-hard enim minim chillwave cloud bread est. Locavore eiusmod deserunt, shaman pariatur normcore poutine wayfarers semiotics vexillologist quis XOXO poke. Enim velit narwhal sustainable proident kinfolk jean shorts. Tote bag farm-to-table yuccie, kogi post-ironic fugiat in cillum." />
+				<div className="content">
+					<ParkImg imagelink="https://rockymountainnationalpark.com/sites/all/themes/rmnp/images/fall-foliage-rocky-mountain-national-park.jpg" />
+					<ParkDesc descript="Lorem ipsum dolor amet umami laboris microdosing copper mug cillum, ethical cornhole. Deserunt XOXO vaporware, kombucha drinking vinegar cronut street art crucifix. Single-origin coffee glossier blue bottle jean shorts iPhone echo park, iceland stumptown laboris tofu dreamcatcher vape. Nulla messenger bag everyday carry fingerstache, asymmetrical try-hard enim minim chillwave cloud bread est. Locavore eiusmod deserunt, shaman pariatur normcore poutine wayfarers semiotics vexillologist quis XOXO poke. Enim velit narwhal sustainable proident kinfolk jean shorts. Tote bag farm-to-table yuccie, kogi post-ironic fugiat in cillum." />
+				</div>
 			</div>
 		);
 	}
